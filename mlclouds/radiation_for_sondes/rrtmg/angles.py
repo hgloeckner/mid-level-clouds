0f34d587035de190980f6c7b84d441d878df0673
Helene Gloeckner <helene.gloeckner@mpimet.mpg.de>
2025-12-10 14:15:44 +0100

    rrtmg sonde calc

---
+++ mlclouds/radiation_for_sondes/rrtmg/angles.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+import pytz
+import timezonefinder
+
+
+def datetime_to_day(time):
+    return time.timetuple().tm_yday
+
+
+def declination_angle_model(d_n):
+    # return in rad
+    a_n = -0.399912
+    b_n = 0.070257
+    theta = 2 * np.pi * d_n / 365
+    return a_n * np.cos(theta) + b_n * np.sin(theta)
+
+
+def get_hour_from_time(time):
+    hour = time.hour
+    minute = time.minute
+    return hour + minute / 60.0
+
+
+def get_h(time):
+    # get hour angle from datetime64 time
+    hour = get_hour_from_time(time)
+    return 15 * (12 - hour)
+
+
+def get_local_time(time, lat, lon):
+    timezone = timezonefinder.TimezoneFinder().timezone_at(lat=lat, lng=lon)
+    tz = pytz.timezone(timezone)
+    return pd.to_datetime(time) + tz.utcoffset(pd.to_datetime(time))
+
+
+def cos_zenith_angle(time, lat, lon):
+    local_time = get_local_time(time, lat, lon)
+
+    declination = declination_angle_model(datetime_to_day(local_time))
+    h = np.deg2rad(get_h(local_time))
+    lat = np.deg2rad(lat)
+
+    return np.sin(lat) * np.sin(declination) + np.cos(lat) * np.cos(
+        declination
+    ) * np.cos(h)
