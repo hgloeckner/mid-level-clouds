8a1448c0e9c9cd0afecd9e3dda0a9008283c30fd
Helene Gloeckner <helene.gloeckner@mpimet.mpg.de>
2025-12-16 10:54:15 +0100

    update helper

--- mlclouds/myutils/physics_helper.py
+++ mlclouds/myutils/physics_helper.py
@@ -1,5 +1,8 @@
 import numpy as np
-import moist_thermodynamics.constants as constants
+import moist_thermodynamics.constants as mtc
+import moist_thermodynamics.utilities as mtu
+import moist_thermodynamics.functions as mtf
+import xarray as xr
 
 
 def get_freezing_levels(ds, vars=None):
@@ -35,7 +38,7 @@ def calc_Tv(T, mr):
     Calculate the virtual temperature (Tv) from temperature (T) and mixing ratio (mr).
     Tv = T * (1 + 0.61 * mr)
     """
-    eps = constants.eps1  # kg / kg
+    eps = mtc.eps1  # kg / kg
     return T * (1 + mr / eps) / (1 + mr)
 
 
@@ -55,9 +58,9 @@ def get_n2(th, qv, altdim="altitude"):
         qv: specific humidity
     """
 
-    Rv = constants.water_vapor_gas_constant
-    Rd = constants.dry_air_gas_constant
-    g = constants.gravity_earth
+    Rv = mtc.water_vapor_gas_constant
+    Rd = mtc.dry_air_gas_constant
+    g = mtc.gravity_earth
     R = Rd + (Rv - Rd) * qv
     dlnthdz = np.log(th).differentiate(altdim)
     dqvdz = qv.differentiate(altdim)
@@ -67,19 +70,19 @@ def get_n2(th, qv, altdim="altitude"):
 
 def get_csc_stab(rho, stability, H):
     grad_stability = stability.differentiate("altitude") * 1000
-    cp = constants.cpv
+    cp = mtc.cpv
     return 1 / (cp * rho * stability) * (H / stability * grad_stability)
 
 
 def get_csc_cooling(rho, stability, H):
     grad_H = H.differentiate("altitude") * 1000
-    cp = constants.cpv
+    cp = mtc.cpv
     return -1 / (cp * rho * stability) * grad_H
 
 
 def density_from_q(p, T, q):
-    Rd = constants.dry_air_gas_constant
-    Rv = constants.water_vapor_gas_constant
+    Rd = mtc.dry_air_gas_constant
+    Rv = mtc.water_vapor_gas_constant
     return p / ((Rd + (Rv - Rd) * q) * T)
 
 
@@ -87,8 +90,8 @@ def wv2q(wv):
     """
     get specific humidity from wales
     """
-    m_l = constants.atomic_mass_dry_air  # g/mol
-    m_w = constants.m_h2o  # g/mol water vapour molar mass
+    m_l = mtc.atomic_mass_dry_air  # g/mol
+    m_w = mtc.m_h2o  # g/mol water vapour molar mass
     C1 = m_w / m_l
     C2 = 1 - C1
     rho_w = wv.wv
@@ -103,3 +106,72 @@ def get_wdir_and_wspd(u, v):
     wdir = (180 + np.arctan2(u, v) * 180 / np.pi) % 360
     wspd = np.sqrt(u**2 + v**2)
     return wdir, wspd
+
+
+def make_sounding_from_adiabat(
+    P, Tsfc=301.0, qsfc=17e-3, Tmin=200.0, thx=mtf.theta_l, integrate=False
+) -> xr.Dataset:
+    """creates a sounding from a moist adiabat
+
+    Cacluates the moist adiabate based either on an integration or a specified
+    isentrope with pressure as the vertical coordinate.
+
+    Args:
+        P: pressure
+        Tsfc: starting (value at P.max()) temperature
+        qsfc: starting (value at P.max()) specific humidity
+        Tmin: minimum temperature of adiabat
+        thx: function to calculate isentrope if integrate = False
+        integrate: determines if explicit integration will be used.
+    """
+
+    TPq = xr.Dataset(
+        data_vars={
+            "T": (
+                ("levels",),
+                mtu.moist_adiabat_with_ice(
+                    P, Tx=Tsfc, qx=qsfc, Tmin=Tmin, thx=thx, integrate=integrate
+                ),
+                {"units": "K", "standard_name": "air_temperature", "symbol": "$T$"},
+            ),
+            "P": (
+                ("levels",),
+                P,
+                {"units": "Pa", "standard_name": "air_pressure", "symbol": "$P$"},
+            ),
+            "q": (
+                ("levels",),
+                qsfc * np.ones(len(P)),
+                {"units": "1", "standard_name": "specific_humidity", "symbol": "$q$"},
+            ),
+        },
+    )
+    TPq = TPq.assign(
+        altitude=xr.DataArray(
+            mtf.pressure_altitude(TPq.P, TPq.T, qv=TPq.q).values,
+            dims=("levels"),
+            attrs={
+                "units": "m",
+                "standard_name": "altitude",
+                "description": "hydrostatic altitude given the datasets temperature and pressure",
+            },
+        )
+    )
+    TPq = TPq.assign(
+        theta=(
+            TPq.T.dims,
+            mtf.theta(TPq.T, TPq.P).values,
+            {
+                "units": "K",
+                "standard_name": "air_potential_teimerature",
+                "symbol": "$\theta$",
+            },
+        )
+    )
+    TPq = TPq.assign(
+        P0=xr.DataArray(
+            mtc.P0, attrs={"units": "Pa", "standards_name": "referenece_pressure"}
+        )
+    )
+
+    return TPq.set_coords("altitude").swap_dims({"levels": "altitude"})
