829c535316037ca9cefdcf80434b8baf8dae9324
Helene Gloeckner <helene.gloeckner@mpimet.mpg.de>
2026-01-28 12:32:24 +0100

    optimize regions

--- mlclouds/cloud_geometry.py
+++ mlclouds/cloud_geometry.py
@@ -9,14 +9,21 @@ from matplotlib.colors import ListedColormap, Normalize
 import sys
 
 sys.path.append("../")
-from myutils import open_datasets
-from myutils import physics_helper as physics
-from myutils.data_helper import sel_sub_domain
+from myutils import open_datasets as od
 import myutils.data_helper as dh
+
+import cartopy.crs as ccrs
+import cartopy.feature as cf
+import matplotlib as mpl
+
+regions = {
+    "north": dh.north,
+    "east": dh.east,
+    "west": dh.west,
+}
 # %%
 
-wv, no_wv = open_datasets.open_wales(masked=False)
-wv = wv.assign(q=physics.wv2q(wv))
+wv, no_wv = od.open_wales(masked=False)
 
 cid = "ipns://latest.orcestra-campaign.org"  # open_datasets.get_cid()
 dropsondes = xr.open_dataset(
@@ -37,7 +44,6 @@ no_wv = xr.where(no_wv.bsrgl_flags == 8, 100, no_wv)
 no_wv = no_wv.where((no_wv.bsrgl_flags == 0) | (no_wv.bsrgl_flags == 8)).sortby(
     "altitude"
 )
-# %%
 
 # %%
 wales = {}
@@ -46,7 +52,7 @@ wales["full"] = xr.DataArray(
     data=dh.find_highest_cloud_altitude(no_wv.sel(altitude=slice(200, None))),
     dims="time",
     name="cloud-top",
-).to_dataframe()
+)
 radar["full"] = xr.DataArray(
     data=dh.find_highest_cloud_altitude(
         rdata.sel(altitude=slice(200, None)),
@@ -55,38 +61,14 @@ radar["full"] = xr.DataArray(
     ),
     dims="time",
     name="cloud-top",
-).to_dataframe()
-
+)
 # %%
-for region, name in zip(
-    [dh.east, dh.west, dh.north],
-    ["east", "west", "north"],
-):
-    wales[name] = xr.DataArray(
-        data=dh.find_highest_cloud_altitude(
-            sel_sub_domain(
-                no_wv, region, item_var="time", lon_var="longitude", lat_var="latitude"
-            )
-        ),
-        dims="time",
-        name="cloud-top",
-    ).to_dataframe()
-    radar[name] = xr.DataArray(
-        data=dh.find_highest_cloud_altitude(
-            sel_sub_domain(
-                rdata.sel(altitude=slice(200, None)),
-                region,
-                item_var="time",
-                lon_var="longitude",
-                lat_var="latitude",
-            ),
-            variable_name="reflectivity",
-            threshold=-1e3,
-        ),
-        dims="time",
-        name="cloud-top",
-    ).to_dataframe()
-
+radar["full"] = radar["full"].assign_coords(
+    time=rdata.time, longitude=rdata.longitude, latitude=rdata.latitude
+)
+wales["full"] = wales["full"].assign_coords(
+    time=no_wv.time, longitude=no_wv.longitude, latitude=no_wv.latitude
+)
 # %%
 sns.set_context("paper", font_scale=1.3)
 fig, ax = plt.subplots(figsize=(8, 5))
@@ -102,8 +84,10 @@ histkwargs = {
     "ax": ax,
     "y": "cloud-top",
 }
-sns.histplot(wales["full"].dropna(), color="k", **histkwargs)
-sns.histplot(radar["full"].dropna(), color="k", linestyle="--", **histkwargs)
+sns.histplot(wales["full"].to_dataframe().dropna(), color="k", **histkwargs)
+sns.histplot(
+    radar["full"].to_dataframe().dropna(), color="k", linestyle="--", **histkwargs
+)
 
 kdekwargs = {
     "fill": False,
@@ -112,10 +96,13 @@ kdekwargs = {
     "y": "cloud-top",
 }
 pt = sns.kdeplot(
-    wales["full"].dropna(), color="k", label="Lidar Cloud-Top", **kdekwargs
+    wales["full"].to_dataframe().dropna(),
+    color="k",
+    label="Lidar Cloud-Top",
+    **kdekwargs,
 )
 pt = sns.kdeplot(
-    radar["full"].dropna(),
+    radar["full"].to_dataframe().dropna(),
     color="k",
     linestyle="--",
     label="Radar Cloud-Top",
@@ -124,26 +111,39 @@ pt = sns.kdeplot(
 """
 for c, name in zip(["#FF7982", "#B6001E", "#00b4d8"], ["north", "east", "west"]):
     sns.kdeplot(
-        wales[name].dropna(),
+        dh.sel_sub_domain(
+            wales["full"],
+            regions[name],
+            item_var="time",
+            lon_var="longitude",
+            lat_var="latitude",
+        ).to_dataframe().dropna(),
         color=c,
         label=f"Wales {name.capitalize()}",
         **kdekwargs,
     )
     sns.kdeplot(
-        radar[name].dropna(),
+        dh.sel_sub_domain(
+            radar["full"],
+            regions[name],
+            item_var="time",
+            lon_var="longitude",
+            lat_var="latitude",
+        ).to_dataframe().dropna(),
         color=c,
         linestyle="--",
         label=f"Radar {name.capitalize()}",
         **kdekwargs,
     )
 """
+
 ax.axhline(5799.8, xmax=0.5, color="k", alpha=0.5)
 ax.axhline(5859.83, xmax=0.5, color="k", linestyle="--", alpha=0.5)
-ax.axhline(6559.85, xmax=0.5, color="#FF7982", alpha=0.5, label="North", linewidth=2)
+ax.axhline(6584.25, xmax=0.5, color="#FF7982", alpha=0.5, label="North", linewidth=2)
 ax.axhline(6363.27, xmax=0.5, linestyle="--", color="#FF7982", alpha=0.5, linewidth=2)
-ax.axhline(5819.00, xmax=0.5, color="#B6001E", alpha=0.5, label="East", linewidth=2)
+ax.axhline(5817.79, xmax=0.5, color="#B6001E", alpha=0.5, label="East", linewidth=2)
 ax.axhline(6005.63, xmax=0.5, linestyle="--", color="#B6001E", alpha=0.5, linewidth=2)
-ax.axhline(5460.19, xmax=0.5, color="#00b4d8", alpha=0.5, label="West", linewidth=2)
+ax.axhline(5389.24, xmax=0.5, color="#00b4d8", alpha=0.5, label="West", linewidth=2)
 ax.axhline(5320.47, xmax=0.5, linestyle="--", color="#00b4d8", alpha=0.5, linewidth=2)
 
 ax.set_yticks(
@@ -171,7 +171,6 @@ fig.savefig(
 )
 # %%
 """
-"""
 region = [
     "full",
     "full",
@@ -195,9 +194,9 @@ for region, height in zip(
     ["north", "east", "west"],
     [6559.80, 5819.43, 5378.91],
 ):
-    ds = sel_sub_domain(
+    ds = dh.sel_sub_domain(
         dropsondes,
-        eval(region),
+        regions[region],
         item_var="sonde",
         lon_var="launch_lon",
         lat_var="launch_lat",
@@ -209,21 +208,75 @@ for region, height in zip(
         ds.ta.sel(altitude=height, method="nearest").mean("sonde").values,
         "K",
     )
-# %%
-
-# %%
-fig, ax = plt.subplots(figsize=(8, 5))
+"""
+# %% cloud top map plot
 
 
-(no_wv.bsrgl.where(no_wv.bsrgl > 20).count(dim="time") / no_wv.sizes["time"]).plot(
-    y="altitude", label="lidar"
+norm = mpl.colors.BoundaryNorm(
+    boundaries=(0, 4000, 6000, 8000, 15000),
+    ncolors=4,
 )
-(
-    rdata.reflectivity.where(rdata.reflectivity > -1e3).count(dim="time")
-    / rdata.sizes["time"]
-).sel(altitude=slice(200, None)).plot(y="altitude", label="radar")
-ax.legend()
+cmap = mpl.colors.ListedColormap(
+    ["#E2E1E2", "#FFC929", "#FF7E15", "#383637"]
+)  # "twilight"#
+cm = 1 / 2.54  # centimeters to inches
+cw = 20 * cm
+fig, axes = plt.subplots(
+    ncols=2, figsize=(cw, cw * 0.5), subplot_kw={"projection": ccrs.Robinson()}
+)
 
+rcoars = radar["full"].reset_coords().resample(time="2S").nearest(tolerance="1S")
+wcoars = (
+    wales["full"]
+    .reset_coords()
+    .resample(time="2S")
+    .nearest(tolerance="1S")
+    .interp(time=rcoars.time)
+)
+coars = xr.DataArray(
+    data=np.nanmax(
+        np.stack([rcoars["cloud-top"].values, wcoars["cloud-top"].values]), axis=0
+    ),
+    dims="time",
+    name="cloud-top",
+).assign_coords(
+    time=rcoars.time,
+    longitude=rcoars.longitude,
+    latitude=rcoars.latitude,
+)
+
+regions = {
+    "east": dh.east,
+    "west": dh.west,
+}
+
+for idx, (name, region) in enumerate(regions.items()):
+    plt_ds = dh.sel_sub_domain(
+        coars, region, lat_var="latitude", lon_var="longitude", item_var="time"
+    )
+    p = axes[idx].scatter(
+        plt_ds.longitude,
+        plt_ds.latitude,
+        c=plt_ds,
+        transform=ccrs.PlateCarree(),
+        alpha=0.5,
+        s=0.5,
+        cmap=cmap,
+        norm=norm,
+    )
+fig.subplots_adjust(right=0.9)
+cbar_ax = fig.add_axes([0.95, 0.3, 0.02, 0.4])
+fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap), cax=cbar_ax)
+
+for ax in axes:
+    ax.add_feature(cf.COASTLINE, linewidth=0.5)
+fig.savefig("/scratch/m/m301046/cloud_top_map_max.pdf")
+# %%
+coars.to_zarr(
+    "/scratch/m/m301046/wales_radar_cloud_top_max.zarr",
+    mode="w",
+    zarr_format=2,
+)
 # %%
 
 date = "2024-08-21"
@@ -237,7 +290,8 @@ dd = rdata.sortby("altitude").sel(time=date, altitude=slice(200, None))
 
 dsdate = dropsondes.swap_dims({"sonde": "launch_time"}).sel(launch_time=date)
 
-# %%
+
+# %% one day radar and wales
 
 fig, axes = plt.subplots(nrows=3, sharex=True, sharey=True, figsize=(12, 8))
 
@@ -332,38 +386,3 @@ crr = fig.colorbar(
 )
 crr.ax.set_ylabel("Reflectivity / dBZ", fontsize=11)
 fig.savefig(f"plots/clouds_{date}.pdf", dpi=300)
-
-# %%
-
-
-rd = rdata.where(rdata.reflectivity >= -1e3)
-ld = no_wv.where(no_wv.bsrgl >= 20)
-# %%
-
-# %%
-print("radar", rd.dropna(dim="time", how="all").sizes)
-print("lidar", ld.dropna(dim="time", how="all").sizes)
-
-# %%
-
-
-# %%
-wales = xr.where(no_wv.bsrgl >= 20, 1, np.nan)
-# %%
-fig, ax = plt.subplots(figsize=(8, 5))
-histkwargs = {
-    "stat": "density",
-    "bins": 30,
-    "binrange": (0, 14000),
-    "element": "step",
-    "fill": False,
-    "linewidth": 1,
-    "kde": False,
-    "alpha": 0.5,
-    "ax": ax,
-    "y": "cloud-top",
-}
-sns.histplot(radar["full"].dropna(), **histkwargs)
-sns.histplot(wales["full"].dropna(), **histkwargs)
-sns.kdeplot(radar["full"].dropna(), **histkwargs)
-sns.kdeplot(wales["full"].dropna(), **histkwargs)
